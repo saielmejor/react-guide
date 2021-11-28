5ad414beadd8603c112a5f6a08153d9cd17ac7ef
Saiken Ho <saikenho@saikens-air.lan>
2021-11-28 17:27:59 -0500

    second commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React , { Component}  from 'react'; 
+
+import './index.css'; // import css into javascript 
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));//select a certain element 
+ 
